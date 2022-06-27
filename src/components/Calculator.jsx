417d109584672f7baea70611329ebe3da6fd574e
kibiku <kibikukimani2@gmail.com>
2022-06-27 17:14:22 +0300

    first commit

---
+++ src/components/Calculator.jsx
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+import BoilingVerdict from "./BoilingVerdict";
+import TemperatureInput from "./TemperatureInput";
+
+class Calculator extends Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = { temperature: "", scale: "c" };
+  }
+
+  handleChange(e) {
+    this.props.onTemperatureChange(e.target.value);
+  }
+
+  handleCelsiusChange = (temperature) => {
+    this.setState({ scale: "c", temperature });
+  };
+
+  handleFahrenheitChange = (temperature) => {
+    this.setState({ scale: "f", temperature });
+  };
+
+  toCelsius = (fahrenheit) => {
+    return ((fahrenheit - 32) * 5) / 9;
+  };
+
+  toFahrenheit = (celsius) => {
+    return (celsius * 9) / 5 + 32;
+  };
+
+  tryConvert = (temperature, convert) => {
+    const input = parseFloat(temperature);
+    if (Number.isNaN(input)) {
+      return "";
+    }
+    const output = convert(input);
+    const rounded = Math.round(output * 1000) / 1000;
+    return rounded.toString();
+  };
+
+  render() {
+    const scale = this.state.scale;
+    const temperature = this.state.temperature;
+    const celsius =
+      scale === "f"
+        ? this.tryConvert(temperature, this.toCelsius)
+        : temperature;
+    const fahrenheit =
+      scale === "c"
+        ? this.tryConvert(temperature, this.toFahrenheit)
+        : temperature;
+    return (
+      <>
+        <TemperatureInput
+          scale="c"
+          temperature={celsius}
+          onTemperatureChange={this.handleCelsiusChange}
+        />
+        <TemperatureInput
+          scale="f"
+          temperature={fahrenheit}
+          onTemperatureChange={this.handleFahrenheitChange}
+        />
+        <BoilingVerdict celsius={parseFloat(celsius)} />
+      </>
+    );
+  }
+}
+
+export default Calculator;
---
+++ src/components/Layout/Footer/Footer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+class Footer extends React.Component {
+  render() {
+    return <footer>Footer</footer>;
+  }
+}
+export default Footer;
