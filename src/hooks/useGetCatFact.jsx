5fefa8e863aafcac70b5807dbf3d1a9fadec913e
kibiku <kibikukim@gmail.com>
2022-07-07 06:22:51 +0300

    more on react hooks

--- src/hooks/useGetCatFact.jsx
+++ src/hooks/useGetCatFact.jsx
@@ -2,12 +2,20 @@ import React, { useEffect, useState } from "react";
 
 export default function useGetCatFact() {
   const [catFact, setCatfact] = useState("");
+  const [name, setName] = useState("");
   useEffect(() => {
     // declare the data fetching function
     const fetchData = async () => {
       const res = await fetch("https://catfact.ninja/fact");
+      const nameRes = await fetch("https://api.agify.io/?name=brian")
+
+
       const catFactNow = await res.json();
       await setCatfact(catFactNow);
+
+
+      const nameNow = await nameRes.json();
+      await setName(nameNow);
     };
 
     // call the function
@@ -15,5 +23,10 @@ export default function useGetCatFact() {
       // make sure to catch any error
       .catch(console.error);
   }, []);
-  return <div>{catFact.fact}</div>;
+  return (
+<>
+    <div>{catFact.fact}</div>
+    <div>{name.name}</div>
+</>
+    )
 }
